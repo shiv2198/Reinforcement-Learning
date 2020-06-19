d7595bab1c7cdfffd03877565c25be1881de79aa
Shivansh Dave <s.k.dave21@gmail.com>
2020-06-19 13:00:16 -0400

    Update Epsilon-greedy.py

--- Epsilon-greedy.py
+++ Epsilon-greedy.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 class Bandit:
     
     
-    def __init__(self,win_rate):
-        self.win_rate = win_rate
+    def __init__(self,v):
+        self.v = v
         self.mean = 0
         self.collected_samples = 0
         
     def pull_arm(self):
-        return np.random.random() < self.win_rate
+        return np.random.randn() + self.v
         
     def update_mean(self,value):
         self.collected_samples += 1
-        self.mean = ( (self.collected_samples - 1)*self.mean + value ) / self.collected_samples
+        self.mean = (1-1/self.collected_samples)*self.mean+1/self.collected_samples*value
             
 def start_game():
             
-    win_rates = [0.25,0.5,0.60]
+    win_rates = [1,2,3]
     total_samples = 10000
-    Eps = 0.08
+    Eps = 0.1
         
     bandits = [Bandit(i) for i in win_rates]
     
@@ -56,7 +56,6 @@ def plot(reward_log,total_samples,win_rates):
     plt.plot(np.ones(total_samples)*win_rates[1])
     plt.plot(np.ones(total_samples)*win_rates[2])
     plt.xscale('log')
-#    plt.plot(np.ones(total_samples)*np.max(win_rates))
     plt.savefig('greedy.png')
             
 if __name__ == "__main__":
